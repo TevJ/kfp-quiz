44604311d93ddeb444c54a42ffcb05a6fe4c1e3c
David <denton.david@gmail.com>
2023-03-19 07:41:05 +0000

    Act: Split application construction into easily digestible chunks, separate from main(). Reason: Separate the transport/runtime from the logic

--- src/main/kotlin/com/techtev/quiz/Application.kt
+++ src/main/kotlin/com/techtev/quiz/Application.kt
@@ -13,6 +13,7 @@ import org.http4k.contract.ui.swaggerUi
 import org.http4k.core.Body
 import org.http4k.core.ContentType
 import org.http4k.core.Credentials
+import org.http4k.core.HttpHandler
 import org.http4k.core.RequestContexts
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.BAD_REQUEST
@@ -21,6 +22,7 @@ import org.http4k.core.then
 import org.http4k.core.with
 import org.http4k.filter.ServerFilters
 import org.http4k.lens.RequestContextKey
+import org.http4k.lens.RequestContextLens
 import org.http4k.lens.string
 import org.http4k.routing.routes
 import org.http4k.server.Jetty
@@ -32,6 +34,10 @@ import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
+    Application().asServer(Jetty(port = 8080)).start()
+}
+
+private fun Application(): HttpHandler {
     Database.connect("jdbc:h2:file:./quizdb", "org.h2.Driver", "sa", "password")
     transaction {
         // print sql to std-out
@@ -40,39 +46,50 @@ fun main() {
         SchemaUtils.createMissingTablesAndColumns(QuizTable)
         SchemaUtils.create(UserTable)
     }
+
     val userRepository = userRepository(userPersistence(UserTable))
     val userService = userService(userRepository)
-    val userApi = contract {
-        renderer = OpenApi3(ApiInfo("User service", "v1.0"), OpenAPIJackson)
-        descriptionPath = "user"
-        routes += userRoutes(userService)
-    }
     val contexts = RequestContexts()
     val credentialsKey = RequestContextKey.required<Credentials>(contexts)
-    val quizApi = contract {
-        renderer = OpenApi3(ApiInfo("Quiz service", "v1.0"), OpenAPIJackson)
-        descriptionPath = "quiz"
-        security = BasicAuthSecurity("kfp-quiz", credentialsKey, { credentials ->
-            userService.isValidUser(credentials.user, credentials.password)
-                .fold({ null }, { credentials })
-        })
-        routes += quizRoutes(
-            quizService(quizRepository(quizPersistence(QuizTable)), userRepository),
-            credentialsKey
-        )
-    }
-    val ui = swaggerUi(
-        descriptionRoute = Uri.of("user"),
-        title = "Quiz service",
-        displayOperationId = true
-    )
-    ServerFilters.InitialiseRequestContext(contexts)
+
+    return ServerFilters.InitialiseRequestContext(contexts)
+        .then(HandleErrors())
         .then(
-            HandleErrors()
+            routes(
+                QuizApi(credentialsKey, userService, userRepository),
+                UserApi(userService),
+                Swagger()
+            )
         )
-        .then(routes(quizApi, userApi, ui))
-        .asServer(Jetty(port = 8080))
-        .start()
+}
+
+private fun Swagger() = swaggerUi(
+    descriptionRoute = Uri.of("user"),
+    title = "Quiz service",
+    displayOperationId = true
+)
+
+private fun UserApi(userService: UserService) = contract {
+    renderer = OpenApi3(ApiInfo("User service", "v1.0"), OpenAPIJackson)
+    descriptionPath = "user"
+    routes += userRoutes(userService)
+}
+
+private fun QuizApi(
+    credentialsKey: RequestContextLens<Credentials>,
+    userService: UserService,
+    userRepository: UserRepository
+) = contract {
+    renderer = OpenApi3(ApiInfo("Quiz service", "v1.0"), OpenAPIJackson)
+    descriptionPath = "quiz"
+    security = BasicAuthSecurity("kfp-quiz", credentialsKey, { credentials ->
+        userService.isValidUser(credentials.user, credentials.password)
+            .fold({ null }, { credentials })
+    })
+    routes += quizRoutes(
+        quizService(quizRepository(quizPersistence(QuizTable)), userRepository),
+        credentialsKey
+    )
 }
 
 private fun HandleErrors() = ServerFilters.CatchLensFailure { failure ->
